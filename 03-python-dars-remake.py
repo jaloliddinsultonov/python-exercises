ea2d7bb5187d648ea10cfeff8949566c2f8babc4
Jaloliddin-Sultonov <94798415+Jaloliddin-Sultonov@users.noreply.github.com>
2022-02-04 23:34:51 +0100

    Create 03-python-dars-remake.py

---
+++ 03-python-dars-remake.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  4 10:15:18 2021
+
+@author: Jaloliddin
+"""
+
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qiladi havas")
+print("5 ning 4-darajasi", 5**4, "ga teng")
+print("22 ni 4 ga bo'lganda", 22%4, "qoldiq qoladi")
+print("Tomonlari 125 ga teng kvadratning yuzi(S=a*a) va perimetrini(P=4*a) ni toping\nS =",125**2, "P=", 4*125)
+
+#Diametri 12 ga teng bo'lgan doiraning yuzini toping(pi=3.14) deb oling
+print("S = pi * r * r\nS=", (3.14*((12/2)**2)))
+
+# Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+print("c=a*a+b*b\nc =",((6**2)+(7**2))**(1/2))
