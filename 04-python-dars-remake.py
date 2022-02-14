6343936e52fd0011db870c4016200446b3ee2052
Jaloliddin-Sultonov <94798415+Jaloliddin-Sultonov@users.noreply.github.com>
2022-02-14 20:19:50 +0100

    Create 04-python-dars-remake.py

---
+++ 04-python-dars-remake.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 21:58:16 2021
+
+@author: Jaloliddin
+"""
+
+name = "Assalomu alaykum"
+print(name)
+name = "Hello World!"
+print(name)
+xabar = "Kichkina demang bizni, ko'tarib uramiz sizni"
+print(xabar)
+xabar = "Katta deng bizni, ko'tarmay uramiz sizni"
+print(xabar)
+
+radius = 5
+pi = 3.14
+aylana_yuzi = pi * radius**2;
+print("Radiusi", radius,"ga teng bo'lgan aylananing yuzi", aylana_yuzi)
